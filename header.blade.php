5dfae8761b2e6b688a41af9a87c85b87b8e7a7e4
kenechukwu <kenufondu@gmail.com>
2020-06-18 04:47:30 -0700

    Add files via upload

---
+++ header.blade.php
@@ -0,0 +1,26 @@
+<header>
+    <nav class="navbar navbar-default">
+    <div class="container-fluid">
+
+        <!--Brand and toggle get grouped  for better mobile dispaly-->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapse" data-target="#bs-example-navbar-collapse" aria-expanded="false">
+                    <span class="sr-only">Toogle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+        </button>
+            <a class="navbar-brand" href="#">Brand</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collaspse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+        </div>
+    </div>
+
+
+
+    </nav>
+
+</header>
